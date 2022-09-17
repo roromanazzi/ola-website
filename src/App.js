07b57a6d2318165229e1e94ad93a111925067ee9
rocio <rocioromanazzi@gmail.com>
2022-09-17 04:54:32 -0300

    te odio react

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import pageStyles from "./App.module.css";
 import ExpansiveCards from "./components/main-page/ExpansiveCards.js";
 import styles from "./components/main-page/ExpansiveCards.module.css";
 import NavBar from "./components/nav-bar/navBar";
+import imgsrc from "./img/individual.jpg";
 
 function App() {
   return (
-    <div className={pageStyles.page}>
+    <div
+      className={pageStyles.page}
+      style={{ backgroundImage: `url(${imgsrc})` }}
+    >
       <div className={styles.cardsContainer}>
         <ExpansiveCards />
       </div>
